78c35f757c7fa256efd8ed73dec5cf5c9ec6151c
okkkk123 <tabishali416@gmail.com>
2024-09-19 01:56:47 -0400

    feat: Implemented homepage layout & design - Added 2 new buttons on homepage - Order Now: routes to the 'Products1' page - Learn More: routes to the 'About Us' page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const navigate = useNavigate();
         <div className="content">
 
         <div className="homepage">
-        <h1>PixelRealm</h1>
+        <h1>PixelRealm Your One Stop for your favourite video games</h1>
         <div className='buttonContainer'>
         <button onClick={handleOrder}>Order Now</button>
         <button onClick={handleLearnMore}>Learn More</button>
