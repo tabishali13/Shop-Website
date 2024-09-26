f6d1fe8d029a5b423ee0795ebead36d142292847
okkkk123 <tabishali416@gmail.com>
2024-09-26 17:11:34 -0400

    feat: Created a data.js file - holds properties for various items - id, product, price, qty, image - was not able to find API so made my own data file

---
+++ src/data.js
@@ -0,0 +1,57 @@
+import PlayStation5 from "/images/ps5 console.jpeg";
+import Playstation5Slim from "/images/PS5 slim.jpeg";
+import NintendoSwitch from "/images/Nintedo Switch base console.jpeg";
+import NintedoSwitchOled from "/images/Nintendo Switch oled.jpeg";
+import XboxSeriesX from "/images/Xbox series x.jpeg";
+import XboxSeriesS from "/images/Xbox series s.jpeg";
+
+
+export default[
+    {
+        id: "Ps5 original",
+        product: "PlayStation 5 console",
+        price: 649.99,
+        qty: 1,
+        image: PlayStation5,
+    },
+
+    {
+        id: "Ps5 slim",
+        product: "PlayStation 5 slim",
+        price: 579.99,
+        qty: 1,
+        image: Playstation5Slim,
+    },
+
+    {
+        id: "Nintedo Switch",
+        product: "Nintendo Switch console",
+        price: 399.94,
+        qty: 1,
+        image: NintendoSwitch,
+    },
+
+    {
+        id: "Nintedo Switch Oled",
+        product: "Nintendo Switch Oled console",
+        price: 449.99,
+        qty: 1,
+        image: NintedoSwitchOled,
+    },
+
+    {
+        id: "Xbox X",
+        product: "Xbox Series X console",
+        price: 649.96,
+        qty: 1,
+        image: XboxSeriesX,
+    },
+
+    {
+        id: "Xbox S",
+        product: "Xbox Series S console",
+        price: 379.99,
+        qty: 1,
+        image: XboxSeriesS,
+    },
+]
