447c635049ffa31c85666d2f99f9b8e721f4cd3e
tabishali13 <tabishali416@gmail.com>
2024-12-02 03:06:42 -0500

    feat: added an Effect to display Cart quantity
    - used CartContext to get myCart state variable
    - displayed it using another state variable on Cart button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../contex/CartContext';
+import Cart from './Cart';
+
 function Navbar() {
+  const { myCart } = useContext(CartContext); //context variable to hold contents of cart
+  const [totalItems, setTotalItems] = useState(0);
   const [select, setSelect] = useState(()=>{
     //either return the page in local storage or 'Home' (on first render)
     return localStorage.getItem("selectedPage") || "Home";
   });
+
+  useEffect(()=>{
+    const total = myCart.reduce((itemTotal, item) => itemTotal + item.qty, 0); // includes 0 as an argument as it represents the initial value 
+    setTotalItems(total);
+  }, [myCart]); //compute total items in myCart whenever myCart changes
 
   const handleMenuChange = (page) => {
     setSelect(page);
@@ -25,6 +35,7 @@ function Navbar() {
   return (
     <>
       <div className= {styles.navContainer}>
+        {/*Make this Clickable so that when clicked brings you back to home page */}
         <div>PixelRealm</div>
         
         <div className= {styles['nav-sections']}>
@@ -37,7 +48,7 @@ function Navbar() {
             <Link to="/shop">Shop</Link> {select === "Shop" ? <hr/> : <></>}  </li>
 
           <li onClick = {()=> handleMenuChange("About")}>
-            <Link to='/About'> About</Link> 
+            <Link to='/About'> About </Link> 
             {select === "About" ? <hr/> : <></>} </li>
 
         </div>
@@ -45,10 +56,9 @@ function Navbar() {
         <div className= {styles.navButtons}>
         
         <Link to= '/Cart'>
-        <button className = {styles['signIn-btn']}> Cart </button>
+        <button className = {styles['signIn-btn']}> Cart({totalItems}) </button>
         </Link>
 
-        <button className= {styles['signup-btn']}>Sign Up</button>
         </div>
 
       </div>
